06f685134d4b63de23ede83e48702c72c7147dbf
tarek <tarek.said.info@gmail.com>
2014-01-24 13:42:21 -0200

    Deleting the awesome-demo as an effort to modularize the project. After the JSFGeneratorImpl is done, I'll start working on the Parser API.

--- Awesome/src/main/java/controllers/GeneratorController.java
+++
@@ -1,7 +0,0 @@
-package controllers;
-
-import models.App;
-
-public interface GeneratorController {
-	void create(App app);
-}
--- Awesome/src/main/java/enums/ActionType.java
+++
@@ -1,9 +0,0 @@
-package enums;
-
-public enum ActionType {
-	DISPLAY,
-	ASSIGN,
-	BUTTON,
-	LINK,
-	CRUD
-}
